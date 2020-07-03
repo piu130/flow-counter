b91ff4e9409dd1edd6a06780c9723c1580262987
Ramon Boss <ramon.boss@outlook.com>
2020-07-03 09:21:47 +0200

    create custom hook for StateFlows

--- src/main/kotlin/welcome.kt
+++ src/main/kotlin/welcome.kt
@@ -1,12 +1,20 @@
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.html.js.onClickFunction
 import react.*
 import react.dom.button
 import react.dom.div
+
+fun <T> useStateFlow(flow: StateFlow<T>): T {
+    val (state, setState) = useState(flow.value)
+
+    useEffectWithCleanup(listOf()) {
+        val job = flow.onEach { setState(it) }.launchIn(GlobalScope)
+        return@useEffectWithCleanup { job.cancel() }
+    }
+    
+    return state
+}
 
 object CounterStore {
     private val _counter = MutableStateFlow(0)
@@ -17,12 +25,7 @@ object CounterStore {
 
 fun RBuilder.welcome(handler: RProps.() -> Unit) = child(welcome) { attrs { handler() } }
 val welcome = functionalComponent<RProps> {
-    val (counter, setCounter) = useState(CounterStore.counter.value)
-
-    useEffectWithCleanup(listOf()) {
-        val job = CounterStore.counter.onEach { setCounter(it) }.launchIn(GlobalScope)
-        return@useEffectWithCleanup { job.cancel() }
-    }
+    val counter = useStateFlow(CounterStore.counter)
 
     div {
         +"Counter: $counter"
